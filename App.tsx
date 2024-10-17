08deb1b2e4501acf913ea4b20d1ee3d732d98c61
Rafinhaa <rafinhaa@me.com>
2024-10-17 12:00:58 -0300

    feat: implement home screen

--- App.tsx
+++ App.tsx
@@ -1,35 +1,27 @@
+import { Inter_700Bold } from "@expo-google-fonts/inter"
 import { useFonts } from "expo-font"
 import { StatusBar } from "expo-status-bar"
-import { useTranslation } from "react-i18next"
-import { Text, View } from "react-native"
-import { createStyleSheet, useStyles } from "react-native-unistyles"
 
+import { ApiProvider } from "@/context/api"
 import "@/locales"
+import Home from "@/screens/home"
+import { api } from "@/services/api"
 import "@/theme"
 
 export default function App() {
   const [loaded] = useFonts({
     "Helvetica-Regular": require("./assets/fonts/HelveticaNeueRegular.otf"),
     "Helvetica-Medium": require("./assets/fonts/HelveticaNeueMedium.otf"),
+    Inter_700Bold,
   })
-  const { t } = useTranslation()
-  const { styles } = useStyles(stylesheet)
 
   if (!loaded) return
 
   return (
-    <View style={styles.container}>
-      <Text>{t("startingMessage")}</Text>
+    <ApiProvider api={api}>
       <StatusBar style="auto" />
-    </View>
+
+      <Home />
+    </ApiProvider>
   )
 }
-
-const stylesheet = createStyleSheet((theme) => ({
-  container: {
-    flex: 1,
-    backgroundColor: theme.colors.gray[0],
-    alignItems: "center",
-    justifyContent: "center",
-  },
-}))
